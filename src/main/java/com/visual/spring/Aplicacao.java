0a7c2b8364d1410225ef347a9da13747b177c446
cristianoboell <cristianoboell@yahoo.com.br>
2018-04-15 17:51:33 -0300

    ADD new

--- src/main/java/com/visual/spring/Aplicacao.java
+++ src/main/java/com/visual/spring/Aplicacao.java
@@ -8,8 +8,6 @@ package com.visual.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,12 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Aplicacao extends SpringBootServletInitializer { 
+public class Aplicacao { 
     public static void main (String[] args) throws Exception{
         SpringApplication.run(Aplicacao.class, args);
     }   
-    
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Aplicacao.class);
-    }
 }
