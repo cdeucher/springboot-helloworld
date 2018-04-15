302dba448756ada01633c54060d3c172d18e5347
cristianoboell <cristianoboell@yahoo.com.br>
2018-04-15 15:33:48 -0300

    remove trash

--- src/main/java/com/visual/spring/Aplicacao.java
+++ src/main/java/com/visual/spring/Aplicacao.java
@@ -16,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Aplicacao {
      
+    
     public static void main (String[] args) throws Exception{
          SpringApplication.run(Aplicacao.class, args);
     }
